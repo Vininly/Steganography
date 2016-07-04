46f78aedf9a06bb1da0364517b50e651d8130ff2
Vinny Sun <Vinny Sun>
2016-07-03 17:32:16 -0700

    renamed file, added small metadata that says how long the message is

--- steg.py
+++ steg.py
@@ -41,17 +41,22 @@ def msgToVec(msg):
 	# for i in range(0, )
 
 def encode(img, msg):
-	""" Insert docstring here """
+	""" Encodes msg in img """
 	assert type(msg) is str
 
 	im = imgToNP(img)
 	h, w, c = np.shape(im)
-
 	im = np.ravel(im)
 
 	asciiArr = [ord(i) for i in msg]
 
-	imPos = 0
+	# Write the length of the message to the first 32 bits
+	msgLen = min(len(msg) * 8, len(im) - 32)
+	for i in range(32):
+		bit = msgLen & (1 << (31 - i))
+		im[i] = setLastBit(im[i], bit)
+
+	imPos = 32
 	for i in asciiArr:
 		for j in xrange(7, -1, -1):
 			bit = i & (1 << j)
@@ -59,8 +64,10 @@ def encode(img, msg):
 				im[imPos] = setLastBit(im[imPos], bit)
 			except:
 				print('Image too small to write entire message')
-				print('Returning truncated version')
-				return im.reshape(h, w, c)
+				print('Returning truncated version\n')
+				im = im.reshape(h, w, c)
+				cv2.imwrite('encode_' + img, im)
+				return
 			imPos += 1
 
 	im = im.reshape(h, w, c)
@@ -78,12 +85,17 @@ def bitsToInt(arr):
 	return res
 
 def decode(img):
-	""" Insert docstring here """
+	""" Decodes the message contained in img """
 
 	im = imgToNP(img)
 
 	# 1 x n array of the last bits of im
 	im = np.ravel(im) & 1
+	# Grab the message length and then remove it from im
+	msgLen = 0
+	for i in range(31, -1, -1):
+		msgLen |= im[31-i] << i
+	im = im[32:msgLen+31]
 	# Make im have a length that's a multiple of 8 by adding 0s
 	im = np.append(im, [0] * (8 - (len(im) % 8)))
 	# Now break it into chunks of 8
@@ -94,12 +106,27 @@ def decode(img):
 	return ''.join(res).strip()
 
 if __name__ == '__main__':
+	# TODO: Add command line arguments?
+
+	action = sys.argv[1]
+	if action != 'e' and action != 'd':
+		print('Invalid action specified, must be encode (e) or decode (d)')
+	else:
+		img = raw_input("Enter your image name: ")
+
+		if action == 'e':
+			txt = raw_input("Enter your text file name: ")
+			with open(txt, 'r') as f:
+				encode(img, f.read())
+		else:
+			print(decode(img))
+
+
 	# encode('black_small.png', 'hi')
 	# print decode('encode_black_small.png')
-	with open('text.txt', 'r') as f:
-		txt = f.read()
-	#print txt
-	encode('xkcd.png', txt)
-	a = decode('encode_xkcd.png')
-	print len(a)
-	print a[:2000]
+	# with open('text.txt', 'r') as f:
+	# 	txt = f.read()
+	# #print txt
+	# encode('xkcd.png', txt)
+	# a = decode('encode_xkcd.png')
+	# print a
